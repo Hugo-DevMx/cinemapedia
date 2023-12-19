06bdf18aab4a45de831e5914eb0fb04516f459de
Lechuguita <computersnew1@gmail.com>
2023-12-19 16:03:19 -0600

    enviromnet variables configured

--- lib/presentation/screens/movies/home_page.dart
+++ lib/presentation/screens/movies/home_page.dart
@@ -1,10 +1,16 @@
+import 'package:cinemapedia/config/constants/enviromnet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Text(Enviromnet.movieDbKey),
+      ),
+    );
   }
 }
